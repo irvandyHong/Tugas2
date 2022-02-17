a6dd8285a15d37e5d8c6a9e051ba840d978ade34
irvandy hartono <irvandyhong@gmail.com>
2022-02-17 14:40:15 +0700

    done maybe?

--- main.go
+++ main.go
@@ -1,12 +1,17 @@
 package main
 
 import (
-	"Tugas2/structs"
 	"database/sql"
 	"fmt"
 
 	_ "github.com/lib/pq"
 )
+
+type orders struct {
+	Order_id      string
+	Ordered_at    string
+	Customer_name string
+}
 
 const (
 	host     = "localhost"
@@ -29,9 +34,14 @@ func main() {
 	defer db.Close()
 	fmt.Println("normal")
 	createOrder(db)
+	//getOrder(db)
+	//deleteOrder(db)
+	updateOrder(db)
+	getOrder(db)
+
 }
 func createOrder(db *sql.DB) {
-	var order = structs.Order{}
+	var order = orders{}
 	sqlStatement := "insert Into orders(order_id,ordered_at,customer_name) Values($1,$2,$3) returning*"
 	err := db.QueryRow(sqlStatement, "order_1", "today", "test1").Scan(&order.Order_id, &order.Ordered_at, &order.Customer_name)
 	if err != nil {
@@ -39,4 +49,46 @@ func createOrder(db *sql.DB) {
 	}
 
 	fmt.Println("New data", order)
+}
+func getOrder(db *sql.DB) {
+	var result = []orders{}
+	sqlStatement := "select * from orders"
+	rows, err := db.Query(sqlStatement)
+	if err != nil {
+		panic(err)
+	}
+	for rows.Next() {
+		var order = orders{}
+
+		err = rows.Scan(&order.Order_id, &order.Ordered_at, &order.Customer_name)
+		if err != nil {
+			panic(err)
+		}
+		result = append(result, order)
+	}
+	fmt.Println("Order Data", result)
+}
+func deleteOrder(db *sql.DB) {
+	sqlStatement := "delete from orders where order_id = $1"
+	res, err := db.Exec(sqlStatement, "order_1")
+	if err != nil {
+		panic(err)
+	}
+	count, err := res.RowsAffected()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("deleted", count)
+}
+func updateOrder(db *sql.DB) {
+	sqlStatement := "update orders set ordered_at = $2, customer_name = $3 where order_id = $1"
+	res, err := db.Exec(sqlStatement, "order_1", "tomorrow", "cust2")
+	if err != nil {
+		panic(err)
+	}
+	count, err := res.RowsAffected()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("affected", count)
 }
